0c7c2212e3073fe52f865de6353dc750e5b11d5b
unknown <parth.k@planpixels.com>
2018-08-06 13:22:14 +0530

    changes in front home page design

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,13 @@ class HomeController extends Controller
         else{
             $photos = '';
         }
-        return view('home',['photos' => $photos, 'title' => $title]);
+        if(\App\Photo::where('approve','yes')->where('show_in_slider','yes')->count()>0)
+        {
+            $section_back_image = \App\Photo::where('approve','yes')->where('show_in_slider','yes')->inRandomOrder()->first();
+        }
+        else{
+            $section_back_image = '';
+        }
+        return view('home',['photos' => $photos, 'title' => $title, 'section_back_image' => $section_back_image]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,11 +36,80 @@
 			{{-- Slider End --}}
 		</div>
 	</div>
-	<div class="container">
-		<div class="row">
-			<h2>What are we...?</h2>
+	<section class="sec-one ptb-60">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="sec-ttl">
+						<h2 data-line="What are we...?">What are we...?</h2>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<h3>Overview</h3>
+							<div class="h3-btm-devider"></div>
+						</div>
+						<div class="col-md-9">
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, but also the leap into electronic typesetting, remaining essentially unchanged when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
+	</section>
+	<section class="sec-two">
+		<div class="table-cell" style="background: url('{{ (isset($section_back_image['photos']))?gallery_photo_url($section_back_image['photos']):'' }}') no-repeat center; background-size: cover;">
+			<div class="col-md-12 table-cell" style="background:rgba(0, 0, 0,0.7);">
+				<div class="centered">
+					<h2>Help Us To Grow</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, incididunt magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, incididunt magna aliqua.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="features1 ptb-60" id="features1-1" data-rv-view="764">
+		<div class="container">
+			<div class="media-container-row">
+				<div class="card p-3 col-12 col-md-6 col-lg-4">
+					<div class="card-img pb-3">
+						<span class="mbr-iconfont mbri-users" style="color: rgb(0, 0, 0);" media-simple="true"></span>
+					</div>
+					<div class="card-box">
+						<h4 class="card-title py-3 mbr-fonts-style display-5">
+							Creativity</h4>
+						<p class="mbr-text mbr-fonts-style display-7">
+							It's the ability to think outside the box. We make decisions, create something new and generate a lot of ideas.
+						</p>
+					</div>
+				</div>
+				<div class="card p-3 col-12 col-md-6 col-lg-4">
+					<div class="card-img pb-3">
+						<span class="mbr-iconfont mbri-globe" style="color: rgb(0, 0, 0);" media-simple="true"></span>
+					</div>
+					<div class="card-box">
+						<h4 class="card-title py-3 mbr-fonts-style display-5">
+							Worldwide</h4>
+						<p class="mbr-text mbr-fonts-style display-7">
+							All sites you make with Mobirise are mobile-friendly. You don't have to create a special mobile version of your site.
+						</p>
+					</div>
+				</div>
+				<div class="card p-3 col-12 col-md-6 col-lg-4">
+					<div class="card-img pb-3">
+						<span class="mbr-iconfont mbri-smile-face" style="color: rgb(0, 0, 0);" media-simple="true"></span>
+					</div>
+					<div class="card-box">
+						<h4 class="card-title py-3 mbr-fonts-style display-5">
+							Unique Styles
+						</h4>
+						<p class="mbr-text mbr-fonts-style display-7">
+							Mobirise offers many site blocks in several themes, and though these blocks are pre-made, they are flexible.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 @endsection
 @push('styles')
 	<style>
@@ -89,6 +158,49 @@
 			}
 		}
 		/* End: style for mail home page slider */
+
+		/* Start: first section - what are we...? */
+		.sec-ttl h2 {
+			text-align: center;
+			position: relative;
+			padding: 25px;
+			font-weight: 700;
+			z-index: 2;
+			color: #000000;
+		}
+		.ptb-60 {
+			padding-top: 60px;
+			padding-bottom: 60px;
+		}
+		/* End: first section - what are we...? */
+
+		/* Start: second section how can you help */
+		.sec-two .table-cell {
+			width: 100%;
+			position: relative;
+			height: 425px;
+			text-align: center;
+		}
+		.sec-two h2 {
+			line-height: 30px;
+			letter-spacing: 2px;
+			color: #e7e7e7;
+			max-width: 350px;
+			margin: 0 auto;
+			font-family: 'Lora', serif;
+			margin-top: 80px;
+		}
+		.sec-two p {
+			font-size: 24px;
+			line-height: 30px;
+			letter-spacing: 2px;
+			color: #e7e7e7;
+			max-width: 900px;
+			margin: 0 auto;
+			font-family: 'Lora', serif;
+			margin-top: 20px;
+		}
+		/* End: second section how can you help */
 	</style>
 @endpush
 @push('scripts')
