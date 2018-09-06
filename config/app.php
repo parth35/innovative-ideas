16561f9caf7f3533fde4f89c6a609382deee78d8
unknown <parth.k@planpixels.com>
2018-09-06 14:08:10 +0530

    new file added for socialite library

--- config/app.php
+++ config/app.php
@@ -160,6 +160,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /* Socialite Login With Social Media */
+        Laravel\Socialite\SocialiteServiceProvider::class
+
     ],
 
     /*
@@ -208,6 +211,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
